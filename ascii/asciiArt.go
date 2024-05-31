d281926ee56ec91523f66c80e27b0a7ae2dae43b
jkuya <kuyajesee@proton.me>
2024-05-31 09:51:54 +0300

    Added the printing to a file in asciiArt

---
+++ ascii/asciiArt.go
@@ -0,0 +1,72 @@
+package ascii
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Ascii prints ASCII art from a given array of characters.
+// The characters are extracted from a predefined file array.
+// The function takes in four arguments: fileArr (a slice of strings representing the file array),
+// wordsArr (a slice of strings representing the words to be printed),
+// lettersToColor (a string representing the letters to be colored),
+// and color (a string representing the color to be applied).
+func Ascii(fileArr []string, wordsArr []string, lettersToColor string, colorCode string, outputfile string) {
+	var count int
+	reset := "\033[0m"
+	if IsFlagPassed("output") {
+		file, err := os.Create(outputfile)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for _, val := range wordsArr {
+			if val != "" {
+				for i := 1; i <= 8; i++ {
+					for _, v := range val {
+						start := (v - 32) * 9
+						if len(lettersToColor) == 0 {
+							fmt.Fprint(file, colorCode+fileArr[int(start)+i]+reset)
+						} else if strings.Contains(lettersToColor, string(v)) {
+							fmt.Fprintf(file, colorCode+fileArr[int(start)+i]+reset)
+						} else {
+							fmt.Fprintf(file, fileArr[int(start)+i])
+						}
+					}
+					fmt.Fprintln(file)
+				}
+			} else {
+				count++
+				if count < len(wordsArr) {
+					fmt.Fprintln(file)
+				}
+			}
+		}
+		return
+	}
+
+	for _, val := range wordsArr {
+		if val != "" {
+			for i := 1; i <= 8; i++ {
+				for _, v := range val {
+					start := (v - 32) * 9
+					if len(lettersToColor) == 0 {
+						fmt.Print(colorCode + fileArr[int(start)+i] + reset)
+					} else if strings.Contains(lettersToColor, string(v)) {
+						fmt.Print(colorCode + fileArr[int(start)+i] + reset)
+					} else {
+						fmt.Print(fileArr[int(start)+i])
+					}
+				}
+				fmt.Println()
+			}
+		} else {
+			count++
+			if count < len(wordsArr) {
+				fmt.Println()
+			}
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"ascii/ascii"
+)
+
+// main function is used to read the banner file and print the ascii art
+// based on the arguments passed
+func main() {
+	for _, v := range os.Args {
+		if v == "--color" || v == "-color" {
+			fmt.Println("Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
+			return
+		}
+	}
+	var filename string
+	var colorflag string
+	var lettersTocolor string
+	var colorCode string
+	var outputFlag string
+
+	flag.StringVar(&outputFlag, "output", "banner.txt", "specify the ascii-font to be printed")
+	flag.StringVar(&colorflag, "color", "reset", "color for color input")
+	flag.Parse()
+	argsPassed := flag.Args()
+
+	if len(argsPassed) == 3 {
+		if !ascii.IsFlagPassed("color") {
+			fmt.Println("Usage: go run . [STRING] [BANNER]\n\nEX: go run . something standard")
+			return
+		}
+		filename = argsPassed[2]
+		argsPassed = argsPassed[:2]
+	} else if len(argsPassed) == 2 {
+		if ascii.IsFlagPassed("color") {
+			_, err := ascii.GetFileName(argsPassed[1])
+			if err != nil {
+				filename = "standard"
+			} else {
+				filename = argsPassed[1]
+				argsPassed = argsPassed[:1]
+			}
+		} else {
+			filename = argsPassed[1]
+			argsPassed = argsPassed[:1]
+		}
+	} else if len(argsPassed) == 1 {
+		filename = "standard"
+	}
+
+	bannerContent, err := ascii.GetFileName(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if ascii.IsFlagPassed("color") {
+		ascii.Color(colorflag, lettersTocolor, argsPassed, bannerContent, outputFlag)
+	} else {
+		colorCode = ""
+		ascii.Art(argsPassed, bannerContent, lettersTocolor, colorCode, 0, outputFlag)
+	}
+}
